48a0c9cec42ff29349e336217f5f396768ff19ac
Sumit Maharjan <sumit.maharjan10@gmail.com>
2025-01-26 08:18:23 +0545

    Faker data generation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,42 +2,37 @@ import { Button, Typography } from '@material-tailwind/react'
 import React from 'react'
 import { useState } from 'react';
 import { TestimonialCard } from '../components/TestimonialCard';
+import { faker } from '@faker-js/faker';
 
 
 
 const Home = () => {
 
-  const [count, setCount] = useState(10);
+  const [users, setUsers] = useState([]);
 
-  const decrement = () => {
-    setCount((prev) => prev - 1);
+  const addUser = () => {
+    const newUser = {
+      userId: faker.string.uuid(),
+      username: faker.internet.username(), // before version 9.1.0, use userName()
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+      }
+
+      console.log(newUser);
     }
-
-  const increment = () => {
-   setCount((prev) => prev + 1);
-   console.log(count);
-  }
-
-  const showEven = count % 2 === 0;
 
   return (
     <div className='p-5'>
 
-      <Typography variant='h3'>{count}</Typography>
+      <Typography variant='h3'>User Listings</Typography>
 
 
       <Button
-        onClick={decrement}
-        disabled={count === 1}
-        size='sm' color='red'>Minus</Button>
-
-      <Button
-        onClick={increment}
-        size='sm' color='light-green'>Plus</Button>
-
-      <div className='mt-5'>
-        {showEven ? <Typography variant='h4'>Even</Typography> : <Typography variant='h4'>Odd</Typography>}
-      </div>
+        onClick={addUser}
+        size='sm' color='green'>Random User</Button>
 
 
     </div>
