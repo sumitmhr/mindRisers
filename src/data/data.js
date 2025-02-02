e98fbf02ad7f68828cc812226fe0f53547d6e2d4
Sumit Maharjan <zerocodeskills07@gmail.com>
2025-02-02 12:48:26 +0545

    TMDB product shown

---
+++ src/data/data.js
@@ -0,0 +1,450 @@
+
+export const movies = {
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/gc8PfyTqzqltKPW3X0cIVUGmagz.jpg",
+      "genre_ids": [
+        18,
+        80
+      ],
+      "id": 1396,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Breaking Bad",
+      "overview": "Walter White, a New Mexico chemistry teacher, is diagnosed with Stage III cancer and given a prognosis of only two years left to live. He becomes filled with a sense of fearlessness and an unrelenting desire to secure his family's financial future at any cost as he enters the dangerous world of drugs and crime.",
+      "popularity": 722.883,
+      "poster_path": "/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg",
+      "first_air_date": "2008-01-20",
+      "name": "Breaking Bad",
+      "vote_average": 8.9,
+      "vote_count": 14933
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/tuCU2yVRM2iZxFkpqlPUyvd6tSu.jpg",
+      "genre_ids": [
+        16,
+        35,
+        10765
+      ],
+      "id": 94954,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Hazbin Hotel",
+      "overview": "In attempt to find a non-violent alternative for reducing Hell's overpopulation, the daughter of Lucifer opens a rehabilitation hotel that offers a group of misfit demons a chance at redemption.",
+      "popularity": 113.105,
+      "poster_path": "/rXojaQcxVUubPLSrFV8PD4xdjrs.jpg",
+      "first_air_date": "2024-01-18",
+      "name": "Hazbin Hotel",
+      "vote_average": 8.8,
+      "vote_count": 1211
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/j67Wi1mzPxPVVybjmaIsvzsqFDH.jpg",
+      "genre_ids": [
+        16,
+        10759,
+        35,
+        10765
+      ],
+      "id": 240411,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "ダンダダン",
+      "overview": "In a bet to prove whether ghosts or aliens exist, two high schoolers face terrifying paranormal threats, gain superpowers and maybe even fall in love?!",
+      "popularity": 233.005,
+      "poster_path": "/6qfZAOEUFIrbUH3JvePclx1nXzz.jpg",
+      "first_air_date": "2024-10-04",
+      "name": "Dan Da Dan",
+      "vote_average": 8.8,
+      "vote_count": 383
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/sYXLeu5usz6yEz0k00FYvtEdodD.jpg",
+      "genre_ids": [
+        16,
+        10765,
+        18,
+        10759
+      ],
+      "id": 94605,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Arcane",
+      "overview": "Amid the stark discord of twin cities Piltover and Zaun, two sisters fight on rival sides of a war between magic technologies and clashing convictions.",
+      "popularity": 298.168,
+      "poster_path": "/abf8tHznhSvl9BAElD2cQeRr7do.jpg",
+      "first_air_date": "2021-11-06",
+      "name": "Arcane",
+      "vote_average": 8.784,
+      "vote_count": 4831
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/96RT2A47UdzWlUfvIERFyBsLhL2.jpg",
+      "genre_ids": [
+        16,
+        10759,
+        10765,
+        18
+      ],
+      "id": 209867,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "葬送のフリーレン",
+      "overview": "After the party of heroes defeated the Demon King, they restored peace to the land and returned to lives of solitude. Generations pass, and the elven mage Frieren comes face to face with humanity’s mortality. She takes on a new apprentice and promises to fulfill old friends’ dying wishes. Can an elven mind make peace with the nature of life and death? Frieren embarks on her quest to find out.",
+      "popularity": 228.502,
+      "poster_path": "/dqZENchTd7lp5zht7BdlqM7RBhD.jpg",
+      "first_air_date": "2023-09-29",
+      "name": "Frieren: Beyond Journey's End",
+      "vote_average": 8.773,
+      "vote_count": 373
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/jvFfHNiCKPWRULvkmeocyjm4QtE.jpg",
+      "genre_ids": [
+        18
+      ],
+      "id": 129888,
+      "origin_country": [
+        "KR"
+      ],
+      "original_language": "ko",
+      "original_name": "스물다섯 스물하나",
+      "overview": "In a time when dreams seem out of reach, a teen fencer pursues big ambitions and meets a hardworking young man who seeks to rebuild his life.",
+      "popularity": 84.896,
+      "poster_path": "/yCQFnmYhYf7XALMka2EoBRAFmPO.jpg",
+      "first_air_date": "2022-02-12",
+      "name": "Twenty Five Twenty One",
+      "vote_average": 8.777,
+      "vote_count": 220
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/lJuQBW4w1x6NgD514xDPqZ2Lbpz.jpg",
+      "genre_ids": [
+        16,
+        18,
+        9648
+      ],
+      "id": 220542,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "薬屋のひとりごと",
+      "overview": "Maomao lived a peaceful life with her apothecary father. Until one day, she's sold as a lowly servant to the emperor's palace. But she wasn't meant for a compliant life among royalty. So when imperial heirs fall ill, she decides to step in and find a cure! This catches the eye of Jinshi, a handsome palace official who promotes her. Now, she's making a name for herself solving medical mysteries!",
+      "popularity": 312.735,
+      "poster_path": "/e3ojpANrFnmJCyeBNTinYwyBCIN.jpg",
+      "first_air_date": "2023-10-22",
+      "name": "The Apothecary Diaries",
+      "vote_average": 8.7,
+      "vote_count": 233
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kU98MbVVgi72wzceyrEbClZmMFe.jpg",
+      "genre_ids": [
+        16,
+        10759,
+        10765
+      ],
+      "id": 246,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Avatar: The Last Airbender",
+      "overview": "In a war-torn world of elemental magic, a young boy reawakens to undertake a dangerous mystic quest to fulfill his destiny as the Avatar, and bring peace to the world.",
+      "popularity": 73.316,
+      "poster_path": "/v2vn1coUMPKw0GI1KGC5J4IXtqp.jpg",
+      "first_air_date": "2005-02-21",
+      "name": "Avatar: The Last Airbender",
+      "vote_average": 8.735,
+      "vote_count": 4209
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/jmV6NIzZxGzRoljFPwao6sh4Nny.jpg",
+      "genre_ids": [
+        18,
+        35
+      ],
+      "id": 259666,
+      "origin_country": [
+        "MX"
+      ],
+      "original_language": "es",
+      "original_name": "Nadie nos va a extrañar",
+      "overview": "It's the '90s, and five losers run an illegal business at their high school. They sell homework and assignments to their classmates. The advantage is that no one ever suspects the nerds. Their business brings them money and popularity. But hormones and an unexpected death threaten to ruin everything. If the business collapses, their futures and their friendships are at risk.",
+      "popularity": 55.788,
+      "poster_path": "/py0s87C9214KRbZ4kWLQfgsE9ET.jpg",
+      "first_air_date": "2024-08-09",
+      "name": "No One Will Miss Us",
+      "vote_average": 8.7,
+      "vote_count": 204
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/4Mt7WHox67uJ1yErwTBFcV8KWgG.jpg",
+      "genre_ids": [
+        10759,
+        35,
+        16
+      ],
+      "id": 37854,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "ワンピース",
+      "overview": "Years ago, the fearsome Pirate King, Gol D. Roger was executed leaving a huge pile of treasure and the famous \"One Piece\" behind. Whoever claims the \"One Piece\" will be named the new King of the Pirates.\n\nMonkey D. Luffy, a boy who consumed a \"Devil Fruit,\" decides to follow in the footsteps of his idol, the pirate Shanks, and find the One Piece. It helps, of course, that his body has the properties of rubber and that he's surrounded by a bevy of skilled fighters and thieves to help him along the way.\n\nLuffy will do anything to get the One Piece and become King of the Pirates!",
+      "popularity": 173.288,
+      "poster_path": "/cMD9Ygz11zjJzAovURpO75Qg7rT.jpg",
+      "first_air_date": "1999-10-20",
+      "name": "One Piece",
+      "vote_average": 8.722,
+      "vote_count": 4768
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/uGy4DCmM33I7l86W7iCskNkvmLD.jpg",
+      "genre_ids": [
+        16,
+        35,
+        10765,
+        10759
+      ],
+      "id": 60625,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Rick and Morty",
+      "overview": "Rick is a mentally-unbalanced but scientifically gifted old man who has recently reconnected with his family. He spends most of his time involving his young grandson Morty in dangerous, outlandish adventures throughout space and alternate universes. Compounded with Morty's already unstable family life, these events cause Morty much distress at home and school.",
+      "popularity": 454.208,
+      "poster_path": "/gdIrmf2DdY5mgN6ycVP0XlzKzbE.jpg",
+      "first_air_date": "2013-12-02",
+      "name": "Rick and Morty",
+      "vote_average": 8.693,
+      "vote_count": 9918
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/A6tMQAo6t6eRFCPhsrShmxZLqFB.jpg",
+      "genre_ids": [
+        10759,
+        16,
+        10765,
+        35
+      ],
+      "id": 31911,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "鋼の錬金術師 FULLMETAL ALCHEMIST",
+      "overview": "Disregard for alchemy’s laws ripped half of Edward Elric’s limbs from his body and left his brother Alphonse’s soul clinging to a suit of armor. To restore what was lost, the brothers seek the Philosopher’s Stone. Enemies and allies – the corrupt military, the Homunculi, and foreign alchemists – will alter the Elric brothers course, but their purpose will remain unchanged and their bond unbreakable.",
+      "popularity": 110.788,
+      "poster_path": "/5ZFUEOULaVml7pQuXxhpR2SmVUw.jpg",
+      "first_air_date": "2009-04-05",
+      "name": "Fullmetal Alchemist: Brotherhood",
+      "vote_average": 8.7,
+      "vote_count": 2107
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/t15KHp3iNfHVQBNIaqUGW12xQA4.jpg",
+      "genre_ids": [
+        80,
+        18
+      ],
+      "id": 60059,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Better Call Saul",
+      "overview": "Six years before Saul Goodman meets Walter White. We meet him when the man who will become Saul Goodman is known as Jimmy McGill, a small-time lawyer searching for his destiny, and, more immediately, hustling to make ends meet. Working alongside, and, often, against Jimmy, is “fixer” Mike Ehrmantraut. The series tracks Jimmy’s transformation into Saul Goodman, the man who puts “criminal” in “criminal lawyer\".",
+      "popularity": 242.187,
+      "poster_path": "/zjg4jpK1Wp2kiRvtt5ND0kznako.jpg",
+      "first_air_date": "2015-02-08",
+      "name": "Better Call Saul",
+      "vote_average": 8.7,
+      "vote_count": 5393
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/20eIP9o5ebArmu2HxJutaBjhLf4.jpg",
+      "genre_ids": [
+        18
+      ],
+      "id": 87108,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Chernobyl",
+      "overview": "The true story of one of the worst man-made catastrophes in history: the catastrophic nuclear accident at Chernobyl. A tale of the brave men and women who sacrificed to save Europe from unimaginable disaster.",
+      "popularity": 122.499,
+      "poster_path": "/hlLXt2tOPT6RRnjiUmoxyG1LTFi.jpg",
+      "first_air_date": "2019-05-06",
+      "name": "Chernobyl",
+      "vote_average": 8.682,
+      "vote_count": 6534
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/iKIyTYNYsQB05EspmjrX468UTmL.jpg",
+      "genre_ids": [
+        10765,
+        18,
+        10759,
+        9648
+      ],
+      "id": 135157,
+      "origin_country": [
+        "KR"
+      ],
+      "original_language": "ko",
+      "original_name": "환혼",
+      "overview": "A powerful sorceress in a blind woman's body encounters a man from a prestigious family, who wants her help to change his destiny.",
+      "popularity": 159.96,
+      "poster_path": "/q2IiPRSXPOZ6qVRj36WRAYEQyHs.jpg",
+      "first_air_date": "2022-06-18",
+      "name": "Alchemy of Souls",
+      "vote_average": 8.7,
+      "vote_count": 631
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/rqbCbjB19amtOtFQbb3K2lgm2zv.jpg",
+      "genre_ids": [
+        16,
+        10765,
+        10759
+      ],
+      "id": 1429,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "進撃の巨人",
+      "overview": "Many years ago, the last remnants of humanity were forced to retreat behind the towering walls of a fortified city to escape the massive, man-eating Titans that roamed the land outside their fortress. Only the heroic members of the Scouting Legion dared to stray beyond the safety of the walls – but even those brave warriors seldom returned alive. Those within the city clung to the illusion of a peaceful existence until the day that dream was shattered, and their slim chance at survival was reduced to one horrifying choice: kill – or be devoured!",
+      "popularity": 130.229,
+      "poster_path": "/hTP1DtLGFamjfu8WqjnuQdP1n4i.jpg",
+      "first_air_date": "2013-04-07",
+      "name": "Attack on Titan",
+      "vote_average": 8.7,
+      "vote_count": 6537
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/ywQtHG17LZhAFZyZtBflhtFMtJ7.jpg",
+      "genre_ids": [
+        18,
+        10751
+      ],
+      "id": 70785,
+      "origin_country": [
+        "CA"
+      ],
+      "original_language": "en",
+      "original_name": "Anne with an E",
+      "overview": "A coming-of-age story about an outsider who, against all odds and numerous challenges, fights for love and acceptance and for her place in the world. The series centers on a young orphaned girl in the late 1890’s, who, after an abusive childhood spent in orphanages and the homes of strangers, is mistakenly sent to live with an elderly woman and her aging brother. Over time, 13-year-old Anne will transform their lives and eventually the small town in which they live with her unique spirit, fierce intellect and brilliant imagination.",
+      "popularity": 104.178,
+      "poster_path": "/6P6tXhjT5tK3qOXzxF9OMLlG7iz.jpg",
+      "first_air_date": "2017-03-19",
+      "name": "Anne with an E",
+      "vote_average": 8.665,
+      "vote_count": 4687
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/2w8FaLwwJTWr6ExUMeVgT2Th5YT.jpg",
+      "genre_ids": [
+        16,
+        35,
+        18,
+        10759
+      ],
+      "id": 42705,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "はじめの一歩",
+      "overview": "Makunouchi Ippo is an ordinary high school student in Japan. Since he spends most of his time away from school helping his mother run the family business, he doesn't get to enjoy his younger years like most teenagers. Always a target for bullying at school (the family fishing business grants him a distinct odor), Ippo's life is one of hardship. One of these after-school bullying sessions turns Ippo's life around for the better, as he is saved by a boxer named Takamura. He decides to follow in Takamura's footsteps and train to become a boxer, giving his life direction and purpose. Ippo's path to perfecting his pugilistic prowess is just beginning...",
+      "popularity": 324.461,
+      "poster_path": "/1LApB9C9kEkh2ZU2vzAhurNDipl.jpg",
+      "first_air_date": "2000-10-03",
+      "name": "Fighting Spirit",
+      "vote_average": 8.7,
+      "vote_count": 1091
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/mGHrUSt2uA5RaIheSmBfRnRHPS8.jpg",
+      "genre_ids": [
+        16,
+        10765,
+        18,
+        10759,
+        35,
+        10762
+      ],
+      "id": 92685,
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "The Owl House",
+      "overview": "An animated fantasy-comedy series that follows Luz, a self-assured teenage girl who accidentally stumbles upon a portal to a magical world where she befriends a rebellious witch, Eda, and an adorably tiny warrior, King. Despite not having magical abilities, Luz pursues her dream of becoming a witch by serving as Eda's apprentice at the Owl House and ultimately finds a new family in an unlikely setting.",
+      "popularity": 136.95,
+      "poster_path": "/zqjSex7DZn7p4dU7mMktdJ8zQV5.jpg",
+      "first_air_date": "2020-01-10",
+      "name": "The Owl House",
+      "vote_average": 8.656,
+      "vote_count": 1624
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/3GQKYh6Trm8pxd2AypovoYQf4Ay.jpg",
+      "genre_ids": [
+        16,
+        10759,
+        10765
+      ],
+      "id": 85937,
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "鬼滅の刃",
+      "overview": "It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon. To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself. Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.",
+      "popularity": 84.918,
+      "poster_path": "/xUfRZu2mi8jH6SzQEJGP6tjBuYj.jpg",
+      "first_air_date": "2019-04-06",
+      "name": "Demon Slayer: Kimetsu no Yaiba",
+      "vote_average": 8.7,
+      "vote_count": 6546
+    }
+  ],
+  "total_pages": 106,
+  "total_results": 2118
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,123 +1,28 @@
-import { Button, Typography } from '@material-tailwind/react'
 import React from 'react'
-import { useState } from 'react';
-import { TestimonialCard } from '../components/TestimonialCard';
-import { faker } from '@faker-js/faker';
-import { TableWithStripedRows } from '../components/TableWithStripedRows';
-
-
+import { movies } from '../data/data'
 
 const Home = () => {
 
 
-    // Collection of comment objects in variable "comments"
-
-    const comments =[
-      {
-        "id": 1,
-        "body": "This is some awesome thinking!",
-        "postId": 242,
-        "likes": 3,
-        "user": {
-          "id": 105,
-          "username": "emmac",
-          "fullName": "Emma Wilson"
-        }
-      },
-      {
-        "id": 2,
-        "body": "What terrific math skills you're showing!",
-        "postId": 46,
-        "likes": 4,
-        "user": {
-          "id": 183,
-          "username": "cameronp",
-          "fullName": "Cameron Perez"
-        }
-      },
-      {
-        "id": 3,
-        "body": "You are an amazing writer!",
-        "postId": 235,
-        "likes": 2,
-        "user": {
-          "id": 1,
-          "username": "emilys",
-          "fullName": "Emily Johnson"
-        }
-      },
-    ]
-
-    // Collection of quote objects in variable "quotes"
-
-
-    const quotes = [
-      {
-        "id": 1,
-        "quote": "Your heart is the size of an ocean. Go find yourself in its hidden depths.",
-        "author": "Rumi"
-      },
-      {
-        "id": 2,
-        "quote": "The Bay of Bengal is hit frequently by cyclones. The months of November and May, in particular, are dangerous in this regard.",
-        "author": "Abdul Kalam"
-      },
-      {
-        "id": 3,
-        "quote": "Thinking is the capital, Enterprise is the way, Hard Work is the solution.",
-        "author": "Abdul Kalam"
-      },
-      {
-        "id": 4,
-        "quote": "If You Can'T Make It Good, At Least Make It Look Good.",
-        "author": "Bill Gates"
-      },
-      {
-        "id": 5,
-        "quote": "Heart be brave. If you cannot be brave, just go. Love's glory is not a small thing.",
-        "author": "Rumi"
-      },
-      {
-        "id": 6,
-        "quote": "It is bad for a young man to sin; but it is worse for an old man to sin.",
-        "author": "Abu Bakr (R.A)"
-      },
-    ];
-  
-  
-
   return (
-    <div className='p-5 space-y-3'>
+    <div className='grid grid-cols-4 gap-4 p-5'>
 
-      <h1>This is comments section:</h1>
+      {
+        movies.results.map(({ name, id, poster_path, overview, vote_average, backdrop_path, }) => {
 
-        {comments.map(({ body, id, likes, postId, user: { username, fullName }}, index) => {
-        return <div key={id}>
-          <p>Body {index + 1} : {body}</p>
-          <h1>Username {index + 1} : {username}</h1>
-
-        </div>
-
-      })}
-
-      <br /><br /><br />
+          return <div key={id}>
+            <img
 
 
+              src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" />
 
-      <h1>This is quotes section:</h1>
-
-        {quotes.map(({id, quote, author}, index) => {
-
-          return <div key = {id}>
-            <p>Quote {index + 1} : {quote}</p>
-            <h1>Author {index + 1} : {author}</h1>
           </div>
-          
-        })}
 
+        })
+      }
 
     </div>
   )
 }
 
-export default Home
+export default Home;
