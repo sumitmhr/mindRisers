64b6757ec2889fd97f1496d4d1c4181748db6f59
Sumit Maharjan <zerocodeskills07@gmail.com>
2025-02-02 14:39:54 +0545

    Get person name without crash even if the value is delayed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,24 +6,37 @@ const Home = () => {
   const [data, setData] = useState();
 
 
+  const [person, setPerson] = useState();
+
   const getData = async () => {
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      // console.log(response.data);
-      setData((prev) => response.data)
-    } catch (err) {
-      console.log(err);
-    }
+    setTimeout(() => {
+      setPerson({ name: 'Sumit' });
+    }, 2000);
+    // try {
+    //   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    //   setData((prev) => response.data)
+    // } catch (err) {
+    //   console.log(err);
+    // }
   }
 
   useEffect(() => {
     getData();
   }, []);
 
+  console.log(person);
+
 
   return (
     <div>
+      {/* "?" = data na aaye samma ko lagi "2000ms"safety. */}
+      <h1>{person?.name}</h1>
 
+       {/* Conditional Rendering
+      <h1>{person && person.name}</h1> */}
+
+      {/* Cannot use this because the value is undefined for 2000ms and browser crashes.
+      <h1>{person.name}</h1>  */}
     </div>
   )
 }
